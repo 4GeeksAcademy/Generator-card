c88402362f60630f55911be873ad06c6222fd999
Alfredo <146830711+JockerAG@users.noreply.github.com>
2023-12-19 17:15:57 +0000

    Generador-de-cartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["&#9824;", "&#9827;", "&#9830;", "&#9829;"];
+  const numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const getRandomElement = Array =>
+    Array[Math.floor(Math.random() * Array.length)];
+
+  const randomPalo = getRandomElement(palos);
+  const randomNumber = getRandomElement(numeros);
+
+  const carta = document.querySelector("#carta");
+  const esquinas = Array.from(carta.querySelectorAll(".esquina"));
+
+  esquinas.map(esquina => {
+    esquina.innerHTML = randomPalo;
+  });
+
+  const numeroElement = carta.querySelector("#number");
+  numeroElement.innerHTML = randomNumber;
 };
